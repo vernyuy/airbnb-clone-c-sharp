034a49a01027c32d1b77fbdbd5d4daa16d726232
vernyuy <fonchu.e.venyuy@gmail.com>
2024-10-22 18:11:37 +0100

    updates

--- src/AirbnbCSharp/AirbnbSharedStack.cs
+++ src/AirbnbCSharp/AirbnbSharedStack.cs
@@ -15,37 +15,65 @@ public class AirbnbSharedStack : Stack
     {
         // Example setup for Cognito User Pool and AppSync API (you can add your actual resources and logic here)
 
-        // DynamoDB Table example setup
-        AirbnbDB = new Table(this, "AirbnbDB", new TableProps
+        // UserPool and UserPool Client
+        var userPool = new UserPool(this, "ACMSCognitoUserPool", new UserPoolProps
         {
-            PartitionKey = new Attribute
+            SelfSignUpEnabled = true,
+            AccountRecovery = AccountRecovery.PHONE_AND_EMAIL,
+            UserVerification = new UserVerificationConfig
             {
-                Name = "PK",
-                Type = AttributeType.STRING
+                EmailStyle = VerificationEmailStyle.CODE
             },
-            SortKey = new Attribute {
-                Name = "SK",
-                Type = AttributeType.STRING
+            AutoVerify = new AutoVerifiedAttrs
+            {
+                Email = true
             },
-            BillingMode = BillingMode.PAY_PER_REQUEST,
-            RemovalPolicy = RemovalPolicy.DESTROY
+            StandardAttributes = new StandardAttributes
+            {
+                Email = new StandardAttribute
+                {
+                    Required = true,
+                    Mutable = true
+                }
+            }
         });
 
-        // AppSync GraphQL API setup example
-        AirbnbApi = new GraphqlApi(this, "AirbnbApi", new GraphqlApiProps
+        var userPoolClient = new UserPoolClient(this, "ACMSUserPoolClient", new UserPoolClientProps
         {
-            Name = "AirbnbApi",
-            Definition = Definition.FromFile(Path.Combine(Directory.GetCurrentDirectory(), "graphql/schema.graphql")),
+            UserPool = userPool
+        });
+
+        // GraphQL API
+        this.AirbnbApi = new GraphqlApi(this, "Api", new GraphqlApiProps
+        {
+            Name = "apartment-complex-management",
+            Schema = SchemaFile.FromAsset("schema/schema.graphql"),
             AuthorizationConfig = new AuthorizationConfig
             {
                 DefaultAuthorization = new AuthorizationMode
                 {
                     AuthorizationType = AuthorizationType.API_KEY
-                }
+                },
+                AdditionalAuthorizationModes = new[]
+                {
+                        new AuthorizationMode
+                        {
+                            AuthorizationType = AuthorizationType.USER_POOL,
+                            UserPoolConfig = new UserPoolConfig
+                            {
+                                UserPool = userPool
+                            }
+                        }
+                    }
             },
-            XrayEnabled = true
+            XrayEnabled = true,
+            LogConfig = new LogConfig
+            {
+                FieldLogLevel = FieldLogLevel.ALL
+            }
         });
 
+        // GraphQL Schema
         // Define the GraphQL Schema for AppSync
         AirbnbApiSchema = new CfnGraphQLSchema(this, "AirbnbApiSchema", new CfnGraphQLSchemaProps
         {
@@ -53,10 +81,62 @@ public class AirbnbSharedStack : Stack
             Definition = File.ReadAllText("graphql/schema.graphql") // Assuming schema is in the graphql folder
         });
 
-        // Example: Output the GraphQL API URL
-        _ = new CfnOutput(this, "GraphQLAPIURL", new CfnOutputProps
+
+        // DynamoDB Table
+        this.AirbnbDB = new Table(this, "ACMSDynamoDbTable", new TableProps
         {
-            Value = AirbnbApi.GraphqlUrl
+            TableName = "AcmsDynamoDBDatabaseTable",
+            PartitionKey = new Attribute
+            {
+                Name = "PK",
+                Type = AttributeType.STRING
+            },
+            SortKey = new Attribute
+            {
+                Name = "SK",
+                Type = AttributeType.STRING
+            },
+            BillingMode = BillingMode.PAY_PER_REQUEST,
+            Stream = StreamViewType.NEW_IMAGE,
+            RemovalPolicy = RemovalPolicy.DESTROY
+        });
+
+        // Adding Global Secondary Index
+        this.AirbnbDB.AddGlobalSecondaryIndex(new GlobalSecondaryIndexProps
+        {
+            IndexName = "getAllApartmentsPerUser",
+            PartitionKey = new Attribute
+            {
+                Name = "GSI1PK",
+                Type = AttributeType.STRING
+            },
+            SortKey = new Attribute
+            {
+                Name = "GSI1SK",
+                Type = AttributeType.STRING
+            },
+            ProjectionType = ProjectionType.ALL
+        });
+
+        // Outputs
+        new CfnOutput(this, "UserPoolId", new CfnOutputProps
+        {
+            Value = userPool.UserPoolId
+        });
+
+        new CfnOutput(this, "UserPoolClientId", new CfnOutputProps
+        {
+            Value = userPoolClient.UserPoolClientId
+        });
+
+        new CfnOutput(this, "GraphQLAPI ID", new CfnOutputProps
+        {
+            Value = this.AirbnbApi.ApiId
+        });
+
+        new CfnOutput(this, "GraphQLAPI URL", new CfnOutputProps
+        {
+            Value = this.AirbnbApi.GraphqlUrl
         });
     }
 }
