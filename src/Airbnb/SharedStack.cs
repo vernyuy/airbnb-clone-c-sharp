48f511bb8562bf0fffa56ec7daeb22e87af91a11
fonchu <fonchu.e.venyuy@gmail.com>
2024-10-22 17:05:04 +0100

    updates

--- src/Airbnb/SharedStack.cs
+++ src/Airbnb/SharedStack.cs
@@ -1,67 +1,3 @@
-// using Amazon.CDK;
-// using Amazon.CDK.AWS.Cognito;
-// using Amazon.CDK.AWS.AppSync;
-// using Amazon.CDK.AWS.DynamoDB;
-// using Constructs;
-// using System.IO;
-
-// public class SharedStack : Stack
-// {
-//     public Table AirbnbDB { get; private set; }
-//     public GraphqlApi AirbnbApi { get; private set; }
-//     public CfnGraphQLSchema AirbnbApiSchema { get; private set; }
-
-//     public SharedStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
-//     {
-//         // Example setup for Cognito User Pool and AppSync API (you can add your actual resources and logic here)
-
-//         // DynamoDB Table example setup
-//         AirbnbDB = new Table(this, "AirbnbDB", new TableProps
-//         {
-//             PartitionKey = new Attribute
-//             {
-//                 Name = "PK",
-//                 Type = AttributeType.STRING
-//             },
-//             SortKey = new Attribute {
-//                 Name = "SK",
-//                 Type = AttributeType.STRING
-//             },
-//             BillingMode = BillingMode.PAY_PER_REQUEST,
-//             RemovalPolicy = RemovalPolicy.DESTROY
-//         });
-
-//         // AppSync GraphQL API setup example
-//         AirbnbApi = new GraphqlApi(this, "AirbnbApi", new GraphqlApiProps
-//         {
-//             Name = "Airbnb",
-//             Definition = Definition.FromFile(Path.Combine(Directory.GetCurrentDirectory(), "graphql/schema.graphql")),
-//             AuthorizationConfig = new AuthorizationConfig
-//             {
-//                 DefaultAuthorization = new AuthorizationMode
-//                 {
-//                     AuthorizationType = AuthorizationType.API_KEY
-//                 }
-//             },
-//             XrayEnabled = true
-//         });
-
-//         // Define the GraphQL Schema for AppSync
-//         AirbnbApiSchema = new CfnGraphQLSchema(this, "AirbnbApiSchema", new CfnGraphQLSchemaProps
-//         {
-//             ApiId = AirbnbApi.ApiId,
-//             Definition = File.ReadAllText("graphql/schema.graphql") // Assuming schema is in the graphql folder
-//         });
-
-//         // Example: Output the GraphQL API URL
-//         _ = new CfnOutput(this, "GraphQLAPIURL", new CfnOutputProps
-//         {
-//             Value = AirbnbApi.GraphqlUrl
-//         });
-//     }
-// }
-
-
 using Amazon.CDK;
 using Amazon.CDK.AWS.Cognito;
 using Amazon.CDK.AWS.AppSync;
@@ -77,10 +13,7 @@ public class SharedStack : Stack
 
     public SharedStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
     {
-        // Example setup for Cognito User Pool and AppSync API (you can add your actual resources and logic here)
-
-        // UserPool and UserPool Client
-        var userPool = new UserPool(this, "ACMSCognitoUserPool", new UserPoolProps
+ var userPool = new UserPool(this, "ACMSCognitoUserPool", new UserPoolProps
         {
             SelfSignUpEnabled = true,
             AccountRecovery = AccountRecovery.PHONE_AND_EMAIL,
@@ -110,8 +43,9 @@ public class SharedStack : Stack
         // GraphQL API
         this.AirbnbApi = new GraphqlApi(this, "Api", new GraphqlApiProps
         {
-            Name = "apartment-complex-management",
-            Schema = SchemaFile.FromAsset("graphql/schema.graphql"),
+            Name = "airbnbApi",
+            Definition = Definition.FromFile(Path.Combine(Directory.GetCurrentDirectory(), "graphql/schema.graphql")),
+            // Schema = SchemaFile.FromAsset("graphql/schema.graphql"),
             AuthorizationConfig = new AuthorizationConfig
             {
                 DefaultAuthorization = new AuthorizationMode
@@ -135,14 +69,6 @@ public class SharedStack : Stack
             {
                 FieldLogLevel = FieldLogLevel.ALL
             }
-        });
-
-        // GraphQL Schema
-        // Define the GraphQL Schema for AppSync
-        AirbnbApiSchema = new CfnGraphQLSchema(this, "AirbnbApiSchema", new CfnGraphQLSchemaProps
-        {
-            ApiId = AirbnbApi.ApiId,
-            Definition = File.ReadAllText("graphql/schema.graphql") // Assuming schema is in the graphql folder
         });
 
 
@@ -202,5 +128,6 @@ public class SharedStack : Stack
         {
             Value = this.AirbnbApi.GraphqlUrl
         });
+      
     }
 }
